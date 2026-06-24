8d329602d726a741182c87ab65302b7ced29b697
Sufyan <sufithetechfurry@gmail.com>
2026-06-24 13:20:46 +0500

    Fixed database configuration to use DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -86,9 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"postgres://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}"
-    )
+    "default": dj_database_url.config(default=config("DATABASE_URL"))
 }
 
 

85c40568b4fb235d63b90cb5eb5727e8df8b7d4c
Sufyan <sufithetechfurry@gmail.com>
2026-06-30 17:22:50 +0500

    Set DEBUG=False for production

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG", "False").strip().lower() in ("true", "1", "yes")
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").strip().lower() in ("true", "1", "yes")
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",

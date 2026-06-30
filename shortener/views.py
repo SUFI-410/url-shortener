03929937c94547662338758f25dd3b32d209efba
Sufyan <sufithetechfurry@gmail.com>
2026-06-30 17:08:33 +0500

    Disable Redis for Railway free plan

--- shortener/views.py
+++ shortener/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from .models import ShortURL
 from .utils import generate_unique_code
-from .redis_client import redis_client
+# from .redis_client import redis_client  # DISABLED - Railway free plan
 from django.shortcuts import redirect
 from django.http import Http404
 
@@ -23,12 +23,12 @@ def shorten_url(request):
         short_code=code
     )
 
-    # NEW: Cache immediately after creation
-    redis_client.set(
-        obj.short_code,
-        obj.original_url,
-        ex=86400
-    )
+    # Redis disabled for Railway free plan
+    # redis_client.set(
+    #     obj.short_code,
+    #     obj.original_url,
+    #     ex=86400
+    # )
 
     return JsonResponse({
         "short_code": obj.short_code,
@@ -40,31 +40,11 @@ def shorten_url(request):
 
 def redirect_url(request, short_code):
 
-    cached_url = redis_client.get(short_code)
-
-    if cached_url:
-        # NEW: Update click count even on cache HIT
-        try:
-            obj = ShortURL.objects.get(short_code=short_code)
-            obj.click_count += 1
-            obj.save()
-        except ShortURL.DoesNotExist:
-            pass
-
-        return redirect(cached_url)
-
+    # Redis disabled - query database directly
     try:
-        obj = ShortURL.objects.get(
-            short_code=short_code
-        )
+        obj = ShortURL.objects.get(short_code=short_code)
 
-        redis_client.set(
-            short_code,
-            obj.original_url,
-            ex=86400
-        )
-
-        # Update click count on database MISS too
+        # Update click count
         obj.click_count += 1
         obj.save()
 

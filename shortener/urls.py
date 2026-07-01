66bee80d89704f525568c1e24d0c1ffc4eb2f72e
Sufyan <sufithetechfurry@gmail.com>
2026-07-01 15:18:08 +0500

    Add HTML form support to shorten_url view

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from .views import shorten_url, redirect_url
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/shorten/', permanent=False), name='home'),
     path("shorten/", shorten_url),
     path("<str:short_code>/", redirect_url),
 ]

f9bc9c94729df746146c5aa911cd135e5a9c72e3
Sufyan <sufithetechfurry@gmail.com>
2026-07-01 15:23:14 +0500

    Add HTML form support to shorten_url view

--- shortener/views.py
+++ shortener/views.py
@@ -1,54 +1,48 @@
 from django.http import JsonResponse
 from .models import ShortURL
 from .utils import generate_unique_code
-# from .redis_client import redis_client  # DISABLED - Railway free plan
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.http import Http404
 
-
 def shorten_url(request):
-
-    url = request.GET.get("url")
-
-    if not url:
-        return JsonResponse(
-            {"error": "url required"},
-            status=400
+    # --- Handle API requests (JSON) ---
+    if request.GET.get("url"):
+        url = request.GET.get("url")
+        code = generate_unique_code()
+        obj = ShortURL.objects.create(
+            original_url=url,
+            short_code=code
         )
-
-    code = generate_unique_code()
-
-    obj = ShortURL.objects.create(
-        original_url=url,
-        short_code=code
-    )
-
-    # Redis disabled for Railway free plan
-    # redis_client.set(
-    #     obj.short_code,
-    #     obj.original_url,
-    #     ex=86400
-    # )
-
-    return JsonResponse({
-        "short_code": obj.short_code,
-        "short_url": request.build_absolute_uri(
-            f"/{obj.short_code}/"
+        return JsonResponse({
+            "short_code": obj.short_code,
+            "short_url": request.build_absolute_uri(f"/{obj.short_code}/")
+        })
+    
+    # --- Handle Web Form (HTML) ---
+    if request.method == 'POST':
+        url = request.POST.get('original_url')
+        if not url:
+            return render(request, 'shortener/index.html', {'error': 'Please enter a URL!'})
+        
+        code = generate_unique_code()
+        obj = ShortURL.objects.create(
+            original_url=url,
+            short_code=code
         )
-    })
+        return render(request, 'shortener/index.html', {
+            'short_url': request.build_absolute_uri(f"/{obj.short_code}/"),
+            'short_code': obj.short_code
+        })
+    
+    # --- GET request (show empty form) ---
+    return render(request, 'shortener/index.html')
 
 
 def redirect_url(request, short_code):
-
-    # Redis disabled - query database directly
     try:
         obj = ShortURL.objects.get(short_code=short_code)
-
-        # Update click count
         obj.click_count += 1
         obj.save()
-
         return redirect(obj.original_url)
-
     except ShortURL.DoesNotExist:
         raise Http404("URL not found")
